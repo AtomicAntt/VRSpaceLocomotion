ac9bf4e3ecb2c99a9468043dfcfd95a8bd3d9908
AtomicAntt <anthonychi36@gmail.com>
2025-04-30 18:42:02 -0700

    Allow user to spawn extinguishers and activate/deactivate menu

--- Assets/Scripts/HandMenuScript.cs
+++ Assets/Scripts/HandMenuScript.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class HandMenuScript : MonoBehaviour
 {
     public Button extinguisherButton;
     public Button checkpointButton;
+    public InputActionProperty activateActionValue;
+
+    public GameObject panel;
 
     public GameObject extinguisher;
+
+    public Transform xrCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +26,20 @@ public class HandMenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float triggerValue = activateActionValue.action.ReadValue<float>();
+        panel.SetActive(triggerValue > 0.1f);
+
     }
 
     public void ExtinguisherButtonPushed()
     {
         print("Extinguisher Button Pushed");
+
+        // Position 1 meter in front of the headset
+        Vector3 spawnPosition = xrCamera.position + xrCamera.forward * 1.0f;
+        Quaternion spawnRotation = Quaternion.LookRotation(xrCamera.forward); // face same direction
+
+        Instantiate(extinguisher, spawnPosition, spawnRotation);
+        //print(activateActionValue.action.ReadValue<float>());
     }
 }
